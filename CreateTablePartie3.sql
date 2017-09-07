ffd3fb7b9a51030b1876ee6e783ff0255ce11c50
kevloppeur93 <arraiskevin@gmail.com>
2017-09-07 21:28:48 +0200

    Add files via upload

---
+++ CreateTablePartie3.sql
@@ -0,0 +1,60 @@
+CREATE TABLE OrderLine (
+ ordLineId numeric identity(1,1) NOT NULL,
+ ordId numeric,
+ booIsbn13 varchar(13),
+ ordLineQuantity int NOT NULL,
+ ordBookPriceHT money NOT NULL,
+ ordBookVAT float NOT NULL
+   
+   
+);
+
+CREATE TABLE Author (
+ autId numeric identity(1,1) NOT NULL,
+ autLastName varchar(50) NOT NULL,
+ autFirstName varchar(50),
+ autBiography varchar(255),
+ autStatusCode tinyint
+  
+   
+);
+
+CREATE TABLE ShippingCost (
+ shipId numeric identity(1,1) NOT NULL,
+ ShipperId numeric,
+ shipName varchar(50) NOT NULL,
+ shipCost float NOT NULL
+   
+   
+);
+
+CREATE TABLE Shipper (
+ ShipperId numeric identity(1,1) NOT NULL,
+ ShipperName varchar(50) NOT NULL
+ );
+
+ CREATE TABLE Formats (
+ forName varchar(50) NOT NULL
+ );
+
+ CREATE TABLE Keywords (
+ keyName varchar(50) NOT NULL
+ );
+
+ CREATE TABLE Possession (
+ forName varchar(50) NOT NULL,
+ booIsbn13 varchar(13) NOT NULL
+ );
+
+  CREATE TABLE Definitions (
+ keyName varchar(50) NOT NULL,
+ booIsbn13 varchar(13) NOT NULL
+ );
+
+  CREATE TABLE BookLanguage (
+ bookLangCode varchar(50) NOT NULL,
+ bookLangName varchar(50) NOT NULL
+ );
+
+
+ 
---
+++ CreateTablePartie4.sql
@@ -0,0 +1,98 @@
+CREATE TABLE Book (
+   booIsbn13 varchar(13) NOT NULL,
+   vatId numeric,
+   revId numeric,
+   ediName varchar(50),
+   booTitle varchar(50) NOT NULL,
+   booSubtitle varchar(50),
+   booPublishYear datetime NOT NULL,
+   booPriceHT money NOT NULL,
+   booResume varchar(255),
+   booQuantity int,
+   booStatus tinyint NOT NULL,
+   booFrontCover varchar(255),
+   booPageNumber int,
+   booLanguage tinyint NOT NULL
+   
+   
+);
+
+
+
+CREATE TABLE Event (
+ eveId numeric identity(1,1) NOT NULL,
+ eveName varchar(50) NOT NULL,
+ eveText varchar(255),
+ eveDateStart datetime NOT NULL,
+ eveDateEnd datetime,
+ eveDiscount float,
+ evePicture varchar(255)
+   
+   
+);
+
+
+
+CREATE TABLE SubTheme (
+subId numeric identity(1,1) NOT NULL,
+theId numeric,
+subName varchar(50) NOT NULL,
+subDescription varchar(255)
+
+);
+
+
+ 
+CREATE TABLE Editor (
+ediName varchar(50) NOT NULL,
+ediPresentation varchar(255),
+ediStatusCode tinyint NOT NULL
+
+);
+
+
+
+CREATE TABLE Writing (
+booIsbn13 varchar(13) NOT NULL,
+autId numeric NOT NULL
+
+);
+
+ 
+CREATE TABLE Have (
+
+eveId numeric NOT NULL,
+booIsbn13 varchar(13) NOT NULL
+
+
+);
+
+
+
+CREATE TABLE Theme (
+
+theId numeric identity(1,1) NOT NULL,
+theName varchar(50) NOT NULL,
+theDescription varchar(255)
+
+
+);
+
+
+
+CREATE TABLE Association (
+
+booIsbn13 varchar(13) NOT NULL,
+subId numeric NOT NULL
+
+);
+
+
+
+CREATE TABLE Vat (
+
+vatId numeric identity(1,1) NOT NULL,
+vatRate float NOT NULL,
+vatName varchar(20) NOT NULL
+
+);
